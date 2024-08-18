887bb868f7ada991972a4399ecd73fd283aebd96
Haris Khan <81975456+Haris-AK@users.noreply.github.com>
2024-08-18 02:14:50 +0100

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 mod user_files;
-use user_files::get_list;
-mod playlist;
-use playlist::generate_playlist;
-mod spotify_data;
-use spotify_data::auth;
+// use user_files::get_list;
 mod user_config;
-use user_config::{read_config, Config};
 
 #[tokio::main]
 async fn main() {
-    let config: Config = read_config();
-    let x = get_list(&config.path);
-    generate_playlist(&x);
-    let info = auth::authenticate_user().await;
-    auth::write_to_toml(info, "Config.toml");
+    // let config: Config = read_config();
+    // let x = get_list(&config.path);
+    // generate_playlist(&x);
+    // let info = auth::authenticate_user().await;
+    // auth::write_to_toml(info, "Config.toml");
+    let x = user_files::get_list(&String::from("/run/media/hak/16TB HDD/Main Music/"));
+    for i in x {
+        println!("{:#?}", i)
+    }
+    let x = vec![1, 2, 3, 4].into_iter();
 }
